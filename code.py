c9dec00690bcea14ac4e97cbbfaecd1cc604e422
shehbaz <sandhushehbaz582@gmail.com>
2026-06-20 19:02:15 +0530

    second version of data

--- code.py
+++ code.py
@@ -8,8 +8,15 @@ df = pd.DataFrame({
     'City': ['New York', 'Los Angeles', 'Chicago', 'Houston']
 })
 
+#adding new row to the dataframe
+new_row = {'Name': 'Eve', 'Age': 28, 'City': 'San Francisco'}
+df.loc[len(df)] = new_row
+
+# making directory
+output_dir = 'data'
+os.makedirs(output_dir, exist_ok=True)
 # SAVE THE DATAFRAME TO A CSV FILE
-output_file = 'output.csv'
+output_file = os.path.join(output_dir, 'output.csv')
 df.to_csv(output_file, index=False)
 # CHECK IF THE FILE EXISTS
 if os.path.exists(output_file):
